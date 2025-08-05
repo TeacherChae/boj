6475a761be52a3fbfd9dccf37b1ab53abafc2666
Keon Chae <brianckh718@gmail.com>
2025-08-05 21:13:21 +0900

    updated #7576

--- Gold/BOJ_#7576.py
+++ Gold/BOJ_#7576.py
@@ -1,56 +1,39 @@
-from collections import deque
 import sys
-
+from collections import deque
 input = sys.stdin.readline
 
-M, N = map(int, input().split())
-basket = []
-rippen_tomatoes = []
-fresh_tomatoes_cnt = 0
-for i in range(N):
-    row = list(map(int, input().split()))
-    for j, c in enumerate(row):
-        if c == 1:
-            rippen_tomatoes.append((i, j))
-        elif c == 0:
-            fresh_tomatoes_cnt += 1
-    basket.append(row)
-if fresh_tomatoes_cnt == 0:
-    print(0)
-if not rippen_tomatoes:
-    print(-1)
-
-def let_tomatoes_have_their_times(matrix, ripen):
+def bfs(matrix, starts, N, M):
     dr = [1, 0, -1, 0]
     dc = [0, 1, 0, -1]
-    r_lim = len(matrix)
-    c_lim = len(matrix[0])
-    def bfs(matrix, ripen):
-        visited = set()
-        queue = deque()
-        for coord in ripen:
-            visited.add(coord)
-            queue.append(coord)
-        count = 0
-        while queue:
-            r, c = queue.popleft()
-            for i in range(4):
-                nr = r + dr[i]
-                nc = c + dc[i]
-                if (nr, nc) not in visited and (0 <= nr < r_lim and 0 <= nc < c_lim):
-                    if matrix[nr][nc] == 0: 
-                        visited.add((nr, nc))
-                        queue.append((nr, nc))
-                        matrix[nr][nc] = 1
-                else:
-                    continue
-            count += 1
-        for i in range(N):
-            for j in range(M):
-                if matrix[i][j] == 0:
-                    return -1
-        return count // len(ripen)
-    
-    return bfs(matrix, ripen)
 
-print(let_tomatoes_have_their_times(basket, rippen_tomatoes))
+    queue = deque()
+    for r, c in starts:
+        queue.append((r, c, 0))  # (행, 열, 날짜)
+
+    max_day = 0
+    while queue:
+        r, c, day = queue.popleft()
+        max_day = max(max_day, day)
+        for i in range(4):
+            nr, nc = r + dr[i], c + dc[i]
+            if 0 <= nr < N and 0 <= nc < M and matrix[nr][nc] == 0:
+                matrix[nr][nc] = 1  # 익히기
+                queue.append((nr, nc, day + 1))
+
+    # 모든 토마토가 익었는지 확인
+    for row in matrix:
+        if 0 in row:
+            return -1
+    return max_day
+
+M, N = map(int, input().split())
+matrix = []
+ripen_tomatoes = []
+for i in range(N):
+    row = list(map(int, input().split()))
+    for j in range(M):
+        if row[j] == 1:
+            ripen_tomatoes.append((i, j))
+    matrix.append(row)
+
+print(bfs(matrix, ripen_tomatoes, N, M))
